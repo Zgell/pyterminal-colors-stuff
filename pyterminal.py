55d1fc184b276c0fd8ddf81484242158759b1e88
Zgell <zgellner2001@gmail.com>
2020-11-26 20:48:09 -0700

    Fixed 8 bit colour test

--- pyterminal.py
+++ pyterminal.py
@@ -16,10 +16,10 @@ class Colours:
     def __init__(self):
         pass
 
-    BLOCK0 = ''
-    BLOCK1 = ''
-    BLOCK2 = ''
-    BLOCK3 = ''
+    BLOCK0 = '\u2591'  # Emptiest block
+    BLOCK1 = '\u2592'  # Semi-empty block
+    BLOCK2 = '\u2593'  # Semi-full block
+    BLOCK3 = '\u2588'  # Fullest block
 
     HEADER = '\033[95m'
     ENDC = '\033[0m'
@@ -28,6 +28,18 @@ class Colours:
         print(n)
 
     def Colour8BitTest():
+        '''
+        Example adapted from resources (1)
+        '''
+        # [NOTE]: Adapt this from O(n^2) to O(n) in the future
+
+        for i in range(0, 16):
+            for j in range(0, 16):
+                string = str(16 * i + j)
+                #sys.stdout.write("\u001b[38;5;"+string+'m '+string.ljust(4))
+                # Coloured Background Test
+                sys.stdout.write("\u001b[48;5;" + string + 'm ' + ' ') #+ string.ljust(4))
+            print("\u001b[0m")  # Reset formatting and end of each line
 
 
 class Colors(Colours):
@@ -36,6 +48,7 @@ class Colors(Colours):
     '''
     def __init__(self):
         super().__init__(self)
+
     def Color8Bit(n):
         #super(Colors, self).Colour8Bit(n)
         Colours.Colour8Bit(n)
@@ -46,9 +59,8 @@ def main():
     Performs a couple of test operations to ensure all of the ANSI codes
     are functioning as intended and are supported on the user's system.
     '''
-    print(Colors.HEADER + 'Header Text' + Colours.ENDC + ' Not Header Text')
-    Colours.Colour8Bit(16)
-    Colors.Color8Bit(32)
+    #print(Colors.HEADER + 'Header Text' + Colours.ENDC + ' Not Header Text')
+    Colours.Colour8BitTest()
     pass
 
 if __name__ == '__main__':
@@ -56,7 +68,8 @@ if __name__ == '__main__':
 
 '''
 References:
-"Build your own Command Line with ANSI escape codes" - Haoyi's Programming 
-Blog
+
+[1] "Build your own Command Line with ANSI escape codes" 
+Haoyi's Programming Blog
 https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html/
 '''
